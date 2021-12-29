43d6b1675b3927d31b2d170b7d34c43afd0190c3
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-29 16:24:15 -0600

    29. Creando un JSON Web Token

--- CRMGraphQl/db/resolvers.js
+++ CRMGraphQl/db/resolvers.js
@@ -1,5 +1,12 @@
 const Usuario = require('../models/Usuarios');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env' });
+
+const crearToken = (usuario, secreta, expiresIn) => {
+   const { id, email, nombre, apellido } = usuario;
+   return jwt.sign({ id, email, nombre, apellido }, secreta, { expiresIn });
+};
 
 const resolvers = {
    Query: {
@@ -26,11 +33,23 @@ const resolvers = {
       },
       autenticarUsuario: async (_, { input }) => {
          const { email, password } = input;
-
+         //Si existe Usuario
          const existeUsuario = await Usuario.findOne({ email });
          if (!existeUsuario) {
             throw new Error('El usuario no existe');
          }
+         //revisar si el password es correcto
+         const passwordCorrecto = await bcryptjs.compare(
+            password,
+            existeUsuario.password
+         );
+         if (!passwordCorrecto) {
+            throw new Error('El password es incorrecto');
+         }
+         //Crear el token
+         return {
+            token: crearToken(existeUsuario, process.env.SECRETA, '24h'),
+         };
       },
    },
 };
