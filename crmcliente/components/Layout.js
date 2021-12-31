bb39d8ef8b39c6099d639f4ec46c72786e5719d4
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-31 15:18:12 -0600

    62. Modificando el Head del Proyecto y agregando Tailwind

--- crmcliente/components/Layout.js
+++ crmcliente/components/Layout.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Head from 'next/head';
+import Sidebar from './Sidebar';
 
 export default function Layout({ children }) {
    return (
       <>
-         <h1>CRM Cliente</h1>
+         <Head>
+            <title>CRM - Administración de Clientes</title>
+            <link
+               rel="stylesheet"
+               href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+               integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+               crossorigin="anonymous"
+               referrerpolicy="no-referrer"
+            />
+            <link
+               rel="stylesheet"
+               href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"
+               integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw=="
+               crossorigin="anonymous"
+               referrerpolicy="no-referrer"
+            />
+         </Head>
+         <Sidebar />
          {children}
       </>
    );
