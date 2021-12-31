e430c927dcc23b11044b0a0fd48ac2301ba47992
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2021-12-31 15:33:21 -0600

    64. Creando el área de contenido principal

--- crmcliente/components/Layout.js
+++ crmcliente/components/Layout.js
@@ -25,7 +25,9 @@ export default function Layout({ children }) {
          <div className="bg-gray-200 min-h-screen">
             <div className="flex minh-screen">
                <Sidebar />
-               {children}
+               <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+                  {children}
+               </main>
             </div>
          </div>
       </>
